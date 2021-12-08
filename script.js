ce5a1285849b1d03e74b49ec4299a53465c57d2a
morticia <morticia@gmail.com>
2021-12-08 18:10:07 +0300

    final

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-$(document).ready(function() {
-    $('.header__burger').click(function(event) {
-     $('.header__burger,.nav').toggleClass('active');
-     $('body').toggleClass('lock')
-    }); 
- }); 
+
+let burder = document.querySelector(".header__burger");
+let nav = document.querySelector("nav");
+let body = document.querySelector("body");
+let activeClass = "active";
+
+burder.addEventListener("click", () => {
+  nav.classList.toggle(activeClass);
+  body.classList.toggle(activeClass);
+}) 
